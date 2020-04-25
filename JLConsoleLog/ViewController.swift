8df420f567bee255b2421f1a973bfe9e9aaedc50
jack <jacklandrin@hotmail.com>
2020-04-25 23:08:20 +0800

    Initial Commit

---
+++ JLConsoleLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JLConsoleLog
+//
+//  Created by jack on 2020/4/25.
+//  Copyright © 2020 jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
